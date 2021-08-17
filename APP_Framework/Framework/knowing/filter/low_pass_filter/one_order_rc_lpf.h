6664b2323485f4b58ef1b88279bcd965faff8068
chunyexixiaoyu <834670833@qq.com>
2021-08-17 16:25:24 +0800

    APP_Framework/Framework/: complete one order RC low pass filter.

--- APP_Framework/Framework/knowing/filter/low_pass_filter/one_order_rc_lpf.h
+++ APP_Framework/Framework/knowing/filter/low_pass_filter/one_order_rc_lpf.h
@@ -2,4 +2,15 @@
 #define _ONE_ORDER_RC_LPF_H
 
 
+typedef struct 
+{
+	float  vi;
+	float  vo_prev;
+	float  vo;
+	float  fcutoff;
+	float  fs;
+} OneOrderRcLpfHander;
+
+void OneOrderRcLpfInit(OneOrderRcLpfHander* hander,float cutoff_fre,float sample);
+float OneOrderRcLpfFun(OneOrderRcLpfHander *hander);
 #endif
