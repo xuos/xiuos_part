43f19c4b55940d83d99246618627e05e92fc6fc7
chunyexixiaoyu <834670833@qq.com>
2021-10-14 11:20:06 +0800

    Ubiquitous/RT_Thread/: add lcd test example update main.c function when it starts.

--- Ubiquitous/RT_Thread/bsp/k210/applications/main.c
+++ Ubiquitous/RT_Thread/bsp/k210/applications/main.c
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2021-10-11 22:04:25
+ * @LastEditTime: 2021-10-14 11:12:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \xiuos\Ubiquitous\RT_Thread\bsp\k210\applications\main.c
+ */
+/*
 * Copyright (c) 2020 AIIT XUOS Lab
 * XiUOS is licensed under Mulan PSL v2.
 * You can use this software according to the terms and conditions of the Mulan PSL v2.
@@ -21,14 +29,13 @@ int main(void)
     rt_thread_mdelay(100);
     char info1[25] ={0};
     char info2[25] ={0};
-    sprintf(info1,"xuos-intelligence k210 build ");
-    sprintf(info2,"%s %s",__DATE__,__TIME__);
+    sprintf(info1,"xuos-intelligence k210  ");
+    sprintf(info2,"build %s %s",__DATE__,__TIME__);
     printf("%s %s \n",info1,info2); 
     #ifdef BSP_USING_LCD
     #include<drv_lcd.h>
-    lcd_clear(PINK); 
-    lcd_draw_string(70,100,info1,BLACK);
-    lcd_draw_string(70,120,info2,BLACK);
+    lcd_show_string(0,60,24,info1);
+    lcd_show_string(0,90,24,info2);
     #endif
     while(1)
     {
