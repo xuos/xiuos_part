de27c59e514c6209f90affef900eeb45d0093acb
chunyexixiaoyu <834670833@qq.com>
2021-12-14 11:11:19 +0800

    Ubiquitous/RT_Thread/: update file system 1.change the tick base on  the board/Kconfig 2.The SD card and FLASH can be mounted to the specified directory of romFS at the same time,and SD card or flash can be used independently 3.Avoid the SD card and Flash mounting sequence. If the Flash mounts the SD card to a specific directory, ensure that the SD card is mounted first

--- Ubiquitous/RT_Thread/bsp/stm32f407-atk-coreboard/board/ports/sdcard_port.c
+++ Ubiquitous/RT_Thread/bsp/stm32f407-atk-coreboard/board/ports/sdcard_port.c
@@ -30,23 +30,27 @@ int sd_mount()
     cs_gpiox = (GPIO_TypeDef *)((rt_base_t)GPIOA + (rt_base_t)(SDCARD_SPI2_CS_PIN / 16) * 0x0400UL);
     cs_pin = (uint16_t)(1 << SDCARD_SPI2_CS_PIN % 16);
     rt_hw_spi_device_attach(SDCARD_SPI_BUS_NAME, "spi20", cs_gpiox, cs_pin);
-    ret = msd_init("sd0", "spi20");
+    ret = msd_init("sd", "spi20");
     if(RT_EOK == ret)
     {
-        if(dfs_mount("sd0", "/", "elm", 0, 0) == 0)
+        #ifdef RT_USING_DFS_ROMFS
+        if(dfs_mount("sd", "/SD", "elm", 0, 0) == 0)
+        #else
+        if(dfs_mount("sd", "/", "elm", 0, 0) == 0)
+        #endif
         {
-            LOG_I("Mount /sd0  successfully"); 
+            LOG_I("Mount sd card  successfully"); 
             return RT_EOK;
         }
         else
         {
-            LOG_E("Mount fail !!1");
-            return -1;
+            LOG_E("Mount fail !!!");
+            return -RT_ERROR;
         }
     }
     LOG_E("msd_init fail !!!");
-    return -2;    
+    return -RT_ERROR;    
 }
-INIT_ENV_EXPORT(sd_mount);
+INIT_APP_EXPORT(sd_mount);
 #endif /* BSP_USING_SDCARD */
 
