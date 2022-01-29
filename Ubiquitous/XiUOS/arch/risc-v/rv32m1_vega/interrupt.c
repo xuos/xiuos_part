d5d7ea74c85bea96616c7d5370b1f4ffbf8fa31c
Wang_Weigen <wangweigen666@163.com>
2022-01-29 15:59:53 +0800

    add rega board interrupt process

--- Ubiquitous/XiUOS/arch/risc-v/rv32m1_vega/interrupt.c
+++ Ubiquitous/XiUOS/arch/risc-v/rv32m1_vega/interrupt.c
@@ -11,7 +11,7 @@
 #include <xs_base.h>
 #include <xs_isr.h>
 #include "fsl_common.h"
-
+#include <RV32M1_ri5cy.h>
 
 int ArchDisableHwIrq(uint32_t irq_num)
 {
@@ -45,6 +45,25 @@ void EnableLocalInterrupt(x_base oldstat)
   __enable_irq();
 }
 
+typedef void (*irq_handler_t)(void);
+extern const irq_handler_t isrTable[];
 
+void SystemIrqHandler(uint32_t mcause)
+{
+    uint32_t intNum;
+
+    if (mcause & 0x80000000) /* For external interrupt. */
+    {
+        intNum = mcause & 0x1FUL;
+
+        /* Clear pending flag in EVENT unit .*/
+        EVENT_UNIT->INTPTPENDCLEAR = (1U << intNum);
+
+        /* Now call the real irq handler for intNum */
+        isrManager.done->incCounter();
+        isrTable[intNum]();
+        isrManager.done->decCounter();
+    }
+}
 
 
