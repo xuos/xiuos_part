c8fc05a0f96e84b06612af661a123a4538ba6647
Liu_Weichao <iacu2016@163.com>
2022-06-24 16:45:15 +0800

    add xiwangtong-arm32 board on XiZi/board/

--- Ubiquitous/XiZi/path_kernel.mk
+++ Ubiquitous/XiZi/path_kernel.mk
@@ -1,6 +1,38 @@
 
 export KERNELPATHS:=
 
+ifeq ($(BSP_ROOT),$(KERNEL_ROOT)/board/xiwangtong-arm32)
+KERNELPATHS :=-I$(BSP_ROOT) \
+	-I$(KERNEL_ROOT)/arch/arm/cortex-m7 \
+	-I$(BSP_ROOT)/third_party_driver \
+	-I$(BSP_ROOT)/third_party_driver/include \
+	-I$(BSP_ROOT)/third_party_driver/sdio/sdmmc/inc \
+	-I$(BSP_ROOT)/third_party_driver/sdio/sdmmc/port \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/host \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/host/class \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/include \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/osa \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/phy \
+	-I$(BSP_ROOT)/third_party_driver/ethernet \
+	-I$(BSP_ROOT)/third_party_driver/ethernet/lan8720 \
+	-I$(BSP_ROOT)/third_party_driver/MIMXRT1052 \
+	-I$(BSP_ROOT)/third_party_driver/MIMXRT1052/drivers \
+	-I$(BSP_ROOT)/third_party_driver/CMSIS/Include \
+	-I$(KERNEL_ROOT)/include \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/compat \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/netif \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip/apps \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip/priv \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip/prot \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/arch \
+	-I$(KERNEL_ROOT)/resources/include \
+	-I$(BSP_ROOT)/include \
+	-I$(BSP_ROOT)/xip #
+endif
+
 ifeq ($(BSP_ROOT),$(KERNEL_ROOT)/board/xidatong)
 KERNELPATHS :=-I$(BSP_ROOT) \
 	-I$(KERNEL_ROOT)/arch/arm/cortex-m7 \
