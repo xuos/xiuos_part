6c80a2da180749af41586ffd78cae22592bb93ca
Liu_Weichao <iacu2016@163.com>
2022-09-06 14:37:00 +0800

    add board/xiwangtong-arm32 Makefile and path_kernel.mk configure

--- Ubiquitous/XiZi/path_kernel.mk
+++ Ubiquitous/XiZi/path_kernel.mk
@@ -55,6 +55,39 @@ KERNELPATHS += \
 endif 
 endif
 
+ifeq ($(BSP_ROOT),$(KERNEL_ROOT)/board/xiwangtong-arm32)
+KERNELPATHS += \
+	-I$(KERNEL_ROOT)/arch/arm/cortex-m7 \
+	-I$(BSP_ROOT)/third_party_driver \
+	-I$(BSP_ROOT)/third_party_driver/include \
+	-I$(BSP_ROOT)/third_party_driver/sdio/sdmmc/inc \
+	-I$(BSP_ROOT)/third_party_driver/sdio/sdmmc/port \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/host \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/host/class \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/include \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/osa \
+	-I$(BSP_ROOT)/third_party_driver/usb/nxp_usb_driver/phy \
+	-I$(BSP_ROOT)/third_party_driver/ethernet \
+	-I$(BSP_ROOT)/third_party_driver/ethernet/lan8720 \
+	-I$(BSP_ROOT)/third_party_driver/CMSIS/Include \
+	-I$(BSP_ROOT)/include \
+	-I$(BSP_ROOT)/xip \
+	-I$(KERNEL_ROOT)/include \
+	-I$(KERNEL_ROOT)/resources/include 
+
+ifeq ($(CONFIG_RESOURCES_LWIP),y)
+KERNELPATHS += \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/compat \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/netif \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip/apps \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip/priv \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/include/lwip/prot \
+	-I$(KERNEL_ROOT)/resources/ethernet/LwIP/arch 
+endif 
+endif
 
 ifeq ($(BSP_ROOT),$(KERNEL_ROOT)/board/xidatong-riscv64)
 KERNELPATHS += \
