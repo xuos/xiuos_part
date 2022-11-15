629e8893e32888e67e225171428001ffcc6784fe
TXuian <1163589503@qq.com>
2022-11-15 07:45:21 -0800

    add w5500 support.

--- Ubiquitous/XiZi_IIoT/board/xidatong-riscv64/third_party_driver/gpio/drv_io_config.c
+++ Ubiquitous/XiZi_IIoT/board/xidatong-riscv64/third_party_driver/gpio/drv_io_config.c
@@ -9,44 +9,49 @@
  */
 
 /**
-* @file drv_io_config.c
-* @brief support xidatong-riscv64-board io configure
-* @version 2.0 
-* @author AIIT XUOS Lab
-* @date 2022-07-25
-*/
+ * @file drv_io_config.c
+ * @brief support xidatong-riscv64-board io configure
+ * @version 2.0
+ * @author AIIT XUOS Lab
+ * @date 2022-07-25
+ */
 
 /*************************************************
 File name: drv_io_config.c
 Description: support kd233-board io configure
 Others: take RT-Thread v4.0.2/bsp/k210/driver/drv_io_config.c for references
                 https://github.com/RT-Thread/rt-thread/tree/v4.0.2
-History: 
+History:
 1. Date: 2022-07-25
 Author: AIIT XUOS Lab
 Modification: support kd233-board io configure
 *************************************************/
 
-#include <xizi.h>
-#include <fpioa.h>
 #include "drv_io_config.h"
-#include <sysctl.h>           
+
+#include <fpioa.h>
+#include <sysctl.h>
+#include <xizi.h>
 
 #define HS_GPIO(n) (FUNC_GPIOHS0 + n)
 
-#define IOCONFIG(pin,func)  {pin, func, #func}
+#define IOCONFIG(pin, func) \
+  { pin, func, #func }
 
-static struct io_config
-{
-    int io_num;
-    fpioa_function_t func;
-    const char * FuncName;
-} io_config[] = 
-{
+static struct io_config {
+  int io_num;
+  fpioa_function_t func;
+  const char* FuncName;
+} io_config[] = {
 #ifdef BSP_USING_LCD
-    IOCONFIG(BSP_LCD_CS_PIN, FUNC_SPI0_SS0),                
-    IOCONFIG(BSP_LCD_WR_PIN, FUNC_SPI0_SCLK),                
-    IOCONFIG(BSP_LCD_DC_PIN, HS_GPIO(LCD_DC_PIN)),     
+    IOCONFIG(BSP_LCD_CS_PIN, FUNC_SPI0_SS0),
+    IOCONFIG(BSP_LCD_WR_PIN, FUNC_SPI0_SCLK),
+    IOCONFIG(BSP_LCD_DC_PIN, HS_GPIO(LCD_DC_PIN)),
+#endif
+
+#ifdef BSP_USING_W5500
+    IOCONFIG(BSP_WIZ_RST_PIN, HS_GPIO(WIZ_RST_PIN)),
+    IOCONFIG(BSP_WIZ_INT_PIN, HS_GPIO(WIZ_INT_PIN)),
 #endif
 
 #ifdef BSP_USING_SPI1
@@ -83,63 +88,59 @@ static struct io_config
 #endif
 
 #ifdef BSP_USING_CH438
-    IOCONFIG(BSP_CH438_ALE_PIN,  HS_GPIO(FPIOA_CH438_ALE)),      
-    IOCONFIG(BSP_CH438_NWR_PIN,  HS_GPIO(FPIOA_CH438_NWR)),      
-    IOCONFIG(BSP_CH438_NRD_PIN,  HS_GPIO(FPIOA_CH438_NRD)),      
-    IOCONFIG(BSP_CH438_INT_PIN,  HS_GPIO(FPIOA_CH438_INT)),      
-    IOCONFIG(BSP_CH438_D0_PIN,  HS_GPIO(FPIOA_CH438_D0)),      
-    IOCONFIG(BSP_CH438_D1_PIN,  HS_GPIO(FPIOA_CH438_D1)),      
-    IOCONFIG(BSP_CH438_D2_PIN,  HS_GPIO(FPIOA_CH438_D2)),      
-    IOCONFIG(BSP_CH438_D3_PIN,  HS_GPIO(FPIOA_CH438_D3)),      
-    IOCONFIG(BSP_CH438_D4_PIN,  HS_GPIO(FPIOA_CH438_D4)),      
-    IOCONFIG(BSP_CH438_D5_PIN,  HS_GPIO(FPIOA_CH438_D5)),      
-    IOCONFIG(BSP_CH438_D6_PIN,  HS_GPIO(FPIOA_CH438_D6)),      
-    IOCONFIG(BSP_CH438_D7_PIN,  HS_GPIO(FPIOA_CH438_D7))
+    IOCONFIG(BSP_CH438_ALE_PIN, HS_GPIO(FPIOA_CH438_ALE)),
+    IOCONFIG(BSP_CH438_NWR_PIN, HS_GPIO(FPIOA_CH438_NWR)),
+    IOCONFIG(BSP_CH438_NRD_PIN, HS_GPIO(FPIOA_CH438_NRD)),
+    IOCONFIG(BSP_CH438_INT_PIN, HS_GPIO(FPIOA_CH438_INT)),
+    IOCONFIG(BSP_CH438_D0_PIN, HS_GPIO(FPIOA_CH438_D0)),
+    IOCONFIG(BSP_CH438_D1_PIN, HS_GPIO(FPIOA_CH438_D1)),
+    IOCONFIG(BSP_CH438_D2_PIN, HS_GPIO(FPIOA_CH438_D2)),
+    IOCONFIG(BSP_CH438_D3_PIN, HS_GPIO(FPIOA_CH438_D3)),
+    IOCONFIG(BSP_CH438_D4_PIN, HS_GPIO(FPIOA_CH438_D4)),
+    IOCONFIG(BSP_CH438_D5_PIN, HS_GPIO(FPIOA_CH438_D5)),
+    IOCONFIG(BSP_CH438_D6_PIN, HS_GPIO(FPIOA_CH438_D6)),
+    IOCONFIG(BSP_CH438_D7_PIN, HS_GPIO(FPIOA_CH438_D7))
 #endif
 };
 
-static int PrintIoConfig()
-{
-    int i;
-    KPrintf("IO Configuration Table\n");
-    KPrintf("┌───────┬────────────────────────┐\n");
-    KPrintf("│Pin    │Function                │\n");
-    KPrintf("├───────┼────────────────────────┤\n");
-    for(i = 0; i < sizeof io_config / sizeof io_config[0]; i++)
-    {
-        KPrintf("│%-2d     │%-24.24s│\n", io_config[i].io_num, io_config[i].FuncName);
-    }
-    KPrintf("└───────┴────────────────────────┘\n");
-    return 0;
+static int PrintIoConfig() {
+  int i;
+  KPrintf("IO Configuration Table\n");
+  KPrintf("┌───────┬────────────────────────┐\n");
+  KPrintf("│Pin    │Function                │\n");
+  KPrintf("├───────┼────────────────────────┤\n");
+  for (i = 0; i < sizeof io_config / sizeof io_config[0]; i++) {
+    KPrintf("│%-2d     │%-24.24s│\n", io_config[i].io_num,
+            io_config[i].FuncName);
+  }
+  KPrintf("└───────┴────────────────────────┘\n");
+  return 0;
 }
-SHELL_EXPORT_CMD(SHELL_CMD_PERMISSION(0)|SHELL_CMD_TYPE(SHELL_TYPE_CMD_FUNC)|SHELL_CMD_PARAM_NUM(0),
-                                                io,PrintIoConfig,print io config);
+SHELL_EXPORT_CMD(SHELL_CMD_PERMISSION(0) | SHELL_CMD_TYPE(SHELL_TYPE_CMD_FUNC) |
+                     SHELL_CMD_PARAM_NUM(0),
+                 io, PrintIoConfig, print io config);
 
-int IoConfigInit(void)
-{
-    int count = sizeof(io_config) / sizeof(io_config[0]);
-    int i;
-    int ret = 0;
+int IoConfigInit(void) {
+  int count = sizeof(io_config) / sizeof(io_config[0]);
+  int i;
+  int ret = 0;
 
-    sysctl_set_power_mode(SYSCTL_POWER_BANK0, SYSCTL_POWER_V18);
-    sysctl_set_power_mode(SYSCTL_POWER_BANK1, SYSCTL_POWER_V18);
-    sysctl_set_power_mode(SYSCTL_POWER_BANK2, SYSCTL_POWER_V18);
+  sysctl_set_power_mode(SYSCTL_POWER_BANK0, SYSCTL_POWER_V18);
+  sysctl_set_power_mode(SYSCTL_POWER_BANK1, SYSCTL_POWER_V18);
+  sysctl_set_power_mode(SYSCTL_POWER_BANK2, SYSCTL_POWER_V18);
 #ifdef BSP_USING_UART2
-    // for IO-27/28
-    sysctl_set_power_mode(SYSCTL_POWER_BANK4, SYSCTL_POWER_V33);
+  // for IO-27/28
+  sysctl_set_power_mode(SYSCTL_POWER_BANK4, SYSCTL_POWER_V33);
 #endif
-#if  defined(BSP_USING_UART1) || defined(BSP_USING_UART3)
-    // for IO-20~23
-    sysctl_set_power_mode(SYSCTL_POWER_BANK3, SYSCTL_POWER_V33);
+#if defined(BSP_USING_UART1) || defined(BSP_USING_UART3)
+  // for IO-20~23
+  sysctl_set_power_mode(SYSCTL_POWER_BANK3, SYSCTL_POWER_V33);
 #endif
 
-    for(i = 0; i < count; i++)
-    {
-        ret = FpioaSetFunction(io_config[i].io_num, io_config[i].func);
-        if(ret != 0)
-            return ret;
-    }
+  for (i = 0; i < count; i++) {
+    ret = FpioaSetFunction(io_config[i].io_num, io_config[i].func);
+    if (ret != 0) return ret;
+  }
 
-    return ret;
+  return ret;
 }
-
