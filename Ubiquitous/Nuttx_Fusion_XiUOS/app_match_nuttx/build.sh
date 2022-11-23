31189c2939a33f60b890d9f16234bfaf1d1c4878
wgzAIIT <820906721@qq.com>
2022-11-23 18:15:53 +0800

    add edu-riscv64 board on nuttx

--- Ubiquitous/Nuttx_Fusion_XiUOS/app_match_nuttx/build.sh
+++ Ubiquitous/Nuttx_Fusion_XiUOS/app_match_nuttx/build.sh
@@ -18,6 +18,7 @@ cp -rf $current/apps $nuttx
 cp -rf $nuttx/aiit_board/aiit-arm32-board  $nuttx/nuttx/boards/arm/stm32
 cp -rf $nuttx/aiit_board/aiit-riscv64-board $nuttx/nuttx/boards/risc-v/k210
 cp -rf $nuttx/aiit_board/xidatong-riscv64 $nuttx/nuttx/boards/risc-v/k210
+cp -rf $nuttx/aiit_board/edu-riscv64 $nuttx/nuttx/boards/risc-v/k210
 cp -rf $nuttx/aiit_board/xidatong-arm32 $nuttx/nuttx/boards/arm/imxrt
 cp -rf $nuttx/aiit_board/hc32f4a0 $nuttx/nuttx/boards/arm/hc32
 
