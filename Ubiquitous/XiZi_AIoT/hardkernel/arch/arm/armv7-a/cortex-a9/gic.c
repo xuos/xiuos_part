6b091797aee0369f7aa51918d33fb0a2572f32e3
Wang_Weigen <wangweigen666@163.com>
2022-12-30 17:59:41 +0800

    1、add 'menuconfig' function for XiZi_AIoT with imx6q-sabrelite board;2、add compilation function for AIoT system with imx6q-sabrelite board;3、add newlib base files;4、add startup files and irq function for imx6q-sabrelite

--- Ubiquitous/XiZi_AIoT/hardkernel/arch/arm/armv7-a/cortex-a9/gic.c
+++ Ubiquitous/XiZi_AIoT/hardkernel/arch/arm/armv7-a/cortex-a9/gic.c
@@ -28,9 +28,9 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include <assert.h>
-#include "core/gic.h"
+#include "gic.h"
 #include "gic_registers.h"
-#include "core/cortex_a9.h"
+#include "cortex_a9.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 // Prototypes
