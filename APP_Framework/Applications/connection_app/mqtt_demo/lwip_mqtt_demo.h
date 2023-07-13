c06138364fbe3231199f205eb53038bf2211c809
huang <1085210385@qq.com>
2023-07-13 10:56:50 +0800

    mqtt update

---
+++ APP_Framework/Applications/connection_app/mqtt_demo/lwip_mqtt_demo.h
@@ -0,0 +1,71 @@
+#include <stdint.h>
+
+#define   MSG_MAX_LEN     500
+#define   MSG_TOPIC_LEN   50
+#define   KEEPLIVE_TIME   50
+#define   MQTT_VERSION    4
+
+#ifdef    LWIP_DNS
+#define   HOST_NAME       "iot-06z00im0uwa0ki2.mqtt.iothub.aliyuncs.com"     //服务器域名
+#else
+#define   HOST_NAME       "iot-06z00im0uwa0ki2.mqtt.iothub.aliyuncs.com"     //服务器IP地址
+#endif
+
+
+//#define   HOST_IP       "129.204.201.235"
+#define   HOST_PORT     1883    //由于是TCP连接，端口必须是1883
+
+#define   CLIENT_ID     "mqttx_24d1dccf"         //随机的id
+#define   USER_NAME     "test"     //用户名
+#define   PASSWORD      "hc123456"  //秘钥
+
+#define   TOPIC         "temp"      //订阅的主题
+
+#define   TEST_MESSAGE  "test_message"  //发送测试消息
+
+enum QoS 
+{ QOS0 = 0, 
+  QOS1, 
+  QOS2 
+};
+
+enum MQTT_Connect
+{
+  Connect_OK = 0,
+  Connect_NOK,
+  Connect_NOTACK
+};
+
+//数据交互结构体
+typedef struct __MQTTMessage
+{
+    uint32_t qos;
+    uint8_t retained;
+    uint8_t dup;
+    uint16_t id;
+	  uint8_t type;
+    void *payload;
+    int32_t payloadlen;
+}MQTTMessage;
+
+//用户接收消息结构体
+typedef struct __MQTT_MSG
+{
+	  uint8_t  msgqos;                 //消息质量
+		uint8_t  msg[MSG_MAX_LEN];       //消息
+	  uint32_t msglenth;               //消息长度
+	  uint8_t  topic[MSG_TOPIC_LEN];   //主题    
+	  uint16_t packetid;               //消息ID
+	  uint8_t  valid;                  //标明消息是否有效
+}MQTT_USER_MSG;
+
+//发送消息结构体
+typedef struct
+{
+    int8_t topic[MSG_TOPIC_LEN];
+    int8_t qos;
+    int8_t retained;
+
+    uint8_t msg[MSG_MAX_LEN];
+    uint8_t msglen;
+} mqtt_recv_msg_t, *p_mqtt_recv_msg_t, mqtt_send_msg_t, *p_mqtt_send_msg_t;
