40cedd339c079d998a8790c2b6609899e126358f
huang <1085210385@qq.com>
2023-09-27 16:21:43 +0800

    add license

--- APP_Framework/Applications/app_test/test_mqttclient/test_mqttclient.c
+++ APP_Framework/Applications/app_test/test_mqttclient/test_mqttclient.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2022 AIIT XUOS Lab
+ * XiUOS is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *        http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+/**
+ * @file test_mqttclient.c
+ * @brief mqtt subscribe fuction test
+ * @version 3.0
+ * @author AIIT XUOS Lab
+ * @date 2023.8.10
+ */
+
 #include <transform.h>
 #include "test_mqttclient.h"
 #include <stdio.h>
