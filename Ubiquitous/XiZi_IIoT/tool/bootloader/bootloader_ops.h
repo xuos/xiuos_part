88e8cc4193a7b01843761acdfb5c1f5f754129f7
Liu_Weichao <iacu2016@163.com>
2023-11-20 10:31:04 +0800

    modify ota function dir

--- Ubiquitous/XiZi_IIoT/tool/bootloader/bootloader_ops.h
+++ Ubiquitous/XiZi_IIoT/tool/bootloader/bootloader_ops.h
@@ -6,14 +6,14 @@
  */
  
 /**
-* @file flash_ops.h
+* @file bootloader_ops.h
 * @brief support flash function
 * @version 2.0 
 * @author AIIT XUOS Lab
 * @date 2023-04-03
 */
-#ifndef __FLASH_OPS_H__
-#define __FLASH_OPS_H__
+#ifndef __BOOTLOADER_OPS_H__
+#define __BOOTLOADER_OPS_H__
 
 
 #include <flash.h>
